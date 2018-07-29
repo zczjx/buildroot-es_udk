1a9334c940c2abe7e85e3c00145f3e69e4864e4f
Clarence.Zhou <zhou_chenz@163.com>
2018-07-29 11:25:21 +0800

    opencv3: use private opencv3 support face detect

--- package/opencv3/opencv3.mk
+++ package/opencv3/opencv3.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-OPENCV3_VERSION = 3.3.0
-OPENCV3_SITE = $(call github,opencv,opencv,$(OPENCV3_VERSION))
+OPENCV3_VERSION = master
+OPENCV3_SITE = git@github.com:zczjx/opencv3.git
+OPENCV3_SITE_METHOD = git2
 OPENCV3_INSTALL_STAGING = YES
 OPENCV3_LICENSE = BSD-3-Clause
 OPENCV3_LICENSE_FILES = LICENSE
