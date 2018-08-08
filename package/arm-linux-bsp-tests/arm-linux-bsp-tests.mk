d2901a3e15c7ce2949e7764930232b94e5f1c2b4
Clarence.Zhou <clarencez@nvidia.com>
2018-08-08 10:43:32 +0800

    bsp tests: use glib2 as STL data structure support

--- package/arm-linux-bsp-tests/arm-linux-bsp-tests.mk
+++ package/arm-linux-bsp-tests/arm-linux-bsp-tests.mk
@@ -10,11 +10,12 @@ ARM_LINUX_BSP_TESTS_SITE_METHOD = git2
 ARM_LINUX_BSP_TESTS_SITE = git@github.com:zczjx/arm-linux-bsp-tests.git
 ARM_LINUX_BSP_TESTS_ALWAYS_BUILD = YES
 ARM_LINUX_BSP_TESTS_INSTALL_STAGING = YES
-ARM_LINUX_BSP_TESTS_DEPENDENCIES +=
+ARM_LINUX_BSP_TESTS_DEPENDENCIES += libglib2
 ARM_LINUX_BSP_TESTS_CFLAGS = -Wall -I $(STAGING_DIR)/usr/include -g \
 							-rdynamic -fPIC  -L$(STAGING_DIR)/usr/lib \
 							-I $(@D)/include -L$(@D)
-ARM_LINUX_BSP_TESTS_LDFLAGS += -lm -lpthread -L$(@D) -lcommon_bsp
+ARM_LINUX_BSP_TESTS_LDFLAGS += -lm -lpthread -L$(@D) -lcommon_bsp \
+	-lglib-2.0 -lgmodule-2.0 -lgobject-2.0 -lgio-2.0 -lgthread-2.0
 ARM_LINUX_BSP_TESTS_TEST_CASE_DIR = $(@D)/bin
 ARM_LINUX_BSP_TESTS_COMMON_DLIB = libcommon_bsp.so
 
@@ -28,6 +29,7 @@ ARM_LINUX_BSP_TESTS_MAKE_FLAGS += \
 		BSP_DLIB=$(ARM_LINUX_BSP_TESTS_COMMON_DLIB)
 
 ifeq ($(BR2_PACKAGE_ARM_LINUX_BSP_TESTS_RPI),y)
+ARM_LINUX_BSP_TESTS_DEPENDENCIES += rpi-firmware rpi-userland
 ARM_LINUX_BSP_TESTS_MAKE_FLAGS += ARM_LINUX_BSP_RPI=y
 else
 ARM_LINUX_BSP_TESTS_MAKE_FLAGS += ARM_LINUX_BSP_RPI=n
