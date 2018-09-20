56e6f05f1756356783a11308904ce6294a60a240
Clarence.Zhou <zhou_chenz@163.com>
2018-09-21 07:26:30 +0800

    add tiny4412-userland support

---
+++ package/tiny4412-userland/tiny4412-userland.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# tiny4412-userland
+#
+################################################################################
+
+TINY4412_USERLAND_VERSION = master
+TINY4412_USERLAND_SITE_METHOD = git2
+TINY4412_USERLAND_SITE = git@github.com:zczjx/tiny4412-userland.git
+TINY4412_USERLAND_LICENSE = Apache-2.0
+TINY4412_USERLAND_LICENSE_FILES = LICENCE
+TINY4412_USERLAND_INSTALL_STAGING = YES
+TINY4412_USERLAND_DEPENDENCIES += 
+
+define TINY4412_USERLAND_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(@D)/etc/init.d/* $(TARGET_DIR)/etc/init.d/
+	$(INSTALL) -m 0755 -D $(@D)/etc/eth0-setting $(TARGET_DIR)/etc/
+
+endef
+
+
+$(eval $(generic-package))
+
